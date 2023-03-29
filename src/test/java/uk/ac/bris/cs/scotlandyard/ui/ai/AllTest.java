dc8caf65e85ce8fec93a996a3eaeecd0704074c4
ec22583 <ec22583@bristol.ac.uk>
2023-03-29 19:45:28 +0100

    Added AllTest file to use for later testing

---
+++ src/test/java/uk/ac/bris/cs/scotlandyard/ui/ai/AllTest.java
@@ -0,0 +1,16 @@
+//Citation: Closed task cw-model
+//Adapted for use in the open task.
+
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Includes all test for the actual game model. WHen we want to run all tests at once
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        DummyTest.class
+})
+public class AllTest {}
