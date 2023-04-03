d8d496c875880b81faaa50e18a8b1e48aabb0264
Thomas Parr <github.us8r1@tpalias.com>
2023-04-03 15:37:51 +0100

    Adds graph.txt to resources

--- src/test/java/uk/ac/bris/cs/scotlandyard/ui/ai/AllTest.java
+++ src/test/java/uk/ac/bris/cs/scotlandyard/ui/ai/AllTest.java
@@ -1,10 +1,12 @@
-//Citation: Closed task cw-model
-//Adapted for use in the open task.
-
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+
+/*
+Citation: Closed task cw-model
+Adapted for use in the open task.
+*/
 
 /**
  * Includes all test for the actual game model. WHen we want to run all tests at once
--- src/test/java/uk/ac/bris/cs/scotlandyard/ui/ai/DummyTest.java
+++ src/test/java/uk/ac/bris/cs/scotlandyard/ui/ai/DummyTest.java
@@ -1,19 +1,18 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
 
-//Taken from the testing modules in closed task
-//https://junit.org/junit4/javadoc/4.12/org/junit/Test.html
-//https://joel-costigliola.github.io/assertj/assertj-core-quick-start.html
+// Taken from the testing modules in closed task
+// https://junit.org/junit4/javadoc/4.12/org/junit/Test.html
+// https://joel-costigliola.github.io/assertj/assertj-core-quick-start.html
 
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
-//Dummy test to prove a concept. Delete this file when the coursework is done
+// Dummy test to prove a concept. Delete this file when the coursework is done
 public class DummyTest {
 
-    @Test
-    public void dummyTest1 (){
+    @Test public void dummyTest1 (){
         int x = 1;
         x++;
     }
