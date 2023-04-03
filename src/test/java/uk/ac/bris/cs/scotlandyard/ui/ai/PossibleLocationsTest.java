1fdc6b0e4f2a62db17cc545117dc1a11f212031a
Thomas Parr <github.us8r1@tpalias.com>
2023-04-03 19:23:40 +0100

    Adds some more complete tests for PossibleLocations

--- src/test/java/uk/ac/bris/cs/scotlandyard/ui/ai/PossibleLocationsTest.java
+++ src/test/java/uk/ac/bris/cs/scotlandyard/ui/ai/PossibleLocationsTest.java
@@ -26,7 +26,7 @@ public class PossibleLocationsTest extends AITestBase{
                 .isEqualTo(0);
     }
 
-    @Test public void test1stDetectiveTurnCorrect () {
+    @Test public void testSingleMovePossiblePositionsCorrect () {
         PossibleLocations possibleLocations = getPossibleLocationsFactory().buildInitialLocations();
         Board.GameState gameState = MyGameStateFactory.a(
                 standard24MoveSetup(),
@@ -41,8 +41,32 @@ public class PossibleLocationsTest extends AITestBase{
 //      Check that it contains the actual location
 //      and not the original locations of Mr X.
         assertThat(possibleLocations.getLocations())
-                .contains(36)
+                .contains(36, 22, 48, 65)
                 .doesNotContainAnyElementsOf(MRX_LOCATIONS)
+
+//              Cannot contain locations of detectives.
+                .doesNotContain(50);
+    }
+
+        @Test public void testDoubleMovePossiblePositionsCorrect () {
+        PossibleLocations possibleLocations = getPossibleLocationsFactory().buildInitialLocations();
+        Board.GameState gameState = MyGameStateFactory.a(
+                standard24MoveSetup(),
+                new Player(MRX, defaultMrXTickets(), 35),
+                ImmutableList.of(new Player(RED, defaultDetectiveTickets(), 50))
+        );
+
+        gameState = gameState.advance(
+                new Move.DoubleMove(MRX, 35, Ticket.TAXI, 22, Ticket.TAXI, 23)
+        );
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+//      Check that it contains the actual location
+//      and all the original locations of Mr X (double move allows Mr X to move back to start).
+        assertThat(possibleLocations.getLocations())
+                .doesNotContain(50)
+                .contains(23)
+                .containsAll(MRX_LOCATIONS)
 
 //              Cannot contain locations of detectives.
                 .doesNotContain(50);
@@ -76,5 +100,124 @@ public class PossibleLocationsTest extends AITestBase{
                 .contains(37)
 //              Check that detective's position is removed.
                 .doesNotContain(66);
+    }
+
+    @Test public void testKnownLocationReducesPossibleLocations () {
+        PossibleLocations possibleLocations = getPossibleLocationsFactory().buildInitialLocations();
+        Board.GameState gameState = MyGameStateFactory.a(
+                standard24MoveSetup(),
+                new Player(MRX, defaultMrXTickets(), 35),
+                ImmutableList.of(new Player(RED, defaultDetectiveTickets(), 50))
+        );
+
+        gameState = gameState.advance(new Move.SingleMove(MRX, 35, Ticket.TAXI, 36));
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+        gameState = gameState.advance(new Move.SingleMove(RED, 50, Ticket.TAXI, 49));
+
+        gameState = gameState.advance(new Move.SingleMove(MRX, 36, Ticket.TAXI, 37));
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+        assertThat(possibleLocations.getLocations())
+//              Has multiple locations before location known.
+                .hasSizeGreaterThan(1);
+
+        gameState = gameState.advance(new Move.SingleMove(RED, 49, Ticket.TAXI, 66));
+
+        gameState = gameState.advance(new Move.SingleMove(MRX, 37, Ticket.TAXI, 50));
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+//      Only contains single correct element.
+        assertThat(possibleLocations.getLocations())
+                .hasSize(1)
+                .contains(50);
+    }
+
+    @Test public void testSingleMoveUpdatesTurnCount () {
+        PossibleLocations possibleLocations = getPossibleLocationsFactory().buildInitialLocations();
+        Board.GameState gameState = MyGameStateFactory.a(
+                standard24MoveSetup(),
+                new Player(MRX, defaultMrXTickets(), 35),
+                ImmutableList.of(new Player(RED, defaultDetectiveTickets(), 50))
+        );
+
+        assertThat(possibleLocations.getTurn())
+                .isEqualTo(0);
+
+        gameState = gameState.advance(new Move.SingleMove(MRX, 35, Ticket.TAXI, 36));
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+        assertThat(possibleLocations.getTurn())
+                .isEqualTo(1);
+    }
+
+        @Test public void testDoubleMoveUpdatesTurnCount () {
+        PossibleLocations possibleLocations = getPossibleLocationsFactory().buildInitialLocations();
+        Board.GameState gameState = MyGameStateFactory.a(
+                standard24MoveSetup(),
+                new Player(MRX, defaultMrXTickets(), 35),
+                ImmutableList.of(new Player(RED, defaultDetectiveTickets(), 50))
+        );
+
+        assertThat(possibleLocations.getTurn())
+                .isEqualTo(0);
+
+        gameState = gameState.advance(
+                new Move.DoubleMove(MRX, 35, Ticket.TAXI, 22, Ticket.TAXI, 23)
+        );
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+        assertThat(possibleLocations.getTurn())
+                .isEqualTo(2);
+    }
+
+    @Test public void testOnlyEdgesWhichMatchTicketAdded () {
+                PossibleLocations possibleLocations = getPossibleLocationsFactory().buildInitialLocations();
+        Board.GameState gameState = MyGameStateFactory.a(
+                standard24MoveSetup(),
+                new Player(MRX, defaultMrXTickets(), 35),
+                ImmutableList.of(new Player(RED, defaultDetectiveTickets(), 50))
+        );
+
+        assertThat(possibleLocations.getTurn())
+                .isEqualTo(0);
+
+        gameState = gameState.advance(new Move.SingleMove(MRX, 35, Ticket.TAXI, 22));
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+        gameState = gameState.advance(new Move.SingleMove(RED, 50, Ticket.TAXI, 49));
+
+        gameState = gameState.advance(new Move.SingleMove(MRX, 22, Ticket.TAXI, 35));
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+        assertThat(possibleLocations.getLocations())
+//              Actual location, as well as all accessible taxi locations from 22.
+                .contains(35, 34, 23, 11)
+//              Would require BUS taken but TAXI ticket used.
+                .doesNotContain(65, 3);
+    }
+
+    @Test public void testSecretTicketAddsAllAdjacentEdges () {
+        PossibleLocations possibleLocations = getPossibleLocationsFactory().buildInitialLocations();
+        Board.GameState gameState = MyGameStateFactory.a(
+                standard24MoveSetup(),
+                new Player(MRX, defaultMrXTickets(), 35),
+                ImmutableList.of(new Player(RED, defaultDetectiveTickets(), 50))
+        );
+
+        assertThat(possibleLocations.getTurn())
+                .isEqualTo(0);
+
+        gameState = gameState.advance(new Move.SingleMove(MRX, 35, Ticket.TAXI, 22));
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+        gameState = gameState.advance(new Move.SingleMove(RED, 50, Ticket.TAXI, 49));
+
+        gameState = gameState.advance(new Move.SingleMove(MRX, 22, Ticket.SECRET, 35));
+        possibleLocations = possibleLocations.updateLocations(gameState);
+
+        assertThat(possibleLocations.getLocations())
+//              Actual location, as well as all accessible locations from 22.
+                .contains(35, 34, 23, 11, 65, 3);
     }
 }
